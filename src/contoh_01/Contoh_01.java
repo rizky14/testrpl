e52031afb66bf09cd0ece2612f47ea631346694c
Rizky <Rizky@Rizky>
2014-12-11 09:05:41 +0700

    upload ke 2 menambahkan output "Halaman Unikom"

--- src/contoh_01/Contoh_01.java
+++ src/contoh_01/Contoh_01.java
@@ -17,6 +17,7 @@ public class Contoh_01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Welcome");
+        System.out.println("Halaman Unikom");
     }
     
 }
